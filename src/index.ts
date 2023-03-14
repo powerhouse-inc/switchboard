62e965f2b11f72f8820f2d202c0844b674e66d27
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-14 15:39:18 +0100

    rm disposable

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,24 @@ import type { Server } from 'http';
 import { createServer as createHttpServer } from 'http';
 import { ApolloServer } from 'apollo-server-express';
 import type express from 'express';
-import type { Disposable } from 'graphql-ws';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import { createApp } from './app';
 import { PORT, isDevelopment } from './env';
 import { createContext } from './context';
 import { schemaWithMiddleware } from './schema';
 
-let serverCleanup: Disposable;
 const application = createApp();
 
-const createApolloServer = (httpServer: Server): ApolloServer => new ApolloServer({
+const createApolloServer = (): ApolloServer => new ApolloServer({
   schema: schemaWithMiddleware,
   context: createContext,
   introspection: isDevelopment,
-  plugins: [
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-    {
-      async serverWillStart() {
-        return {
-          async drainServer() {
-            serverCleanup?.dispose();
-          },
-        };
-      },
-    },
-  ],
 });
 
 const startServer = async (
   app: express.Application,
 ): Promise<Server> => {
   const httpServer = createHttpServer(app);
-  const apollo = createApolloServer(httpServer);
+  const apollo = createApolloServer();
   await apollo.start();
   apollo.applyMiddleware({ app });
   return httpServer.listen({ port: PORT }, () => {
