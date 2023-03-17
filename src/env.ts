89477f42a3ba92910c75d36900e2f5d756bfc801
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-17 11:54:23 +0100

    lint

--- src/env.ts
+++ src/env.ts
@@ -7,7 +7,7 @@ if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined');
   }
 }
-export const JWT_SECRET = process.env.JWT_SECRET || "dev";
+export const JWT_SECRET = process.env.JWT_SECRET || 'dev';
 export const PORT = Number(process.env.PORT ?? '3000');
 export const isDevelopment = process.env.NODE_ENV === 'development';
 export const AUTH_SIGNUP_ENABLED = Boolean(process.env.AUTH_SIGNUP_ENABLED);
