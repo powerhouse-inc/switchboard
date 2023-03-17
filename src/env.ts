ef6c1730c6074b44c21d0279954f91660a5f6005
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-17 11:53:06 +0100

    fix secret set

--- src/env.ts
+++ src/env.ts
@@ -3,9 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 if (!process.env.JWT_SECRET) {
-  throw new Error('JWT_SECRET is not defined');
+  if (process.env.NODE_ENV === 'production') {
+    throw new Error('JWT_SECRET is not defined');
+  }
 }
-export const { JWT_SECRET } = process.env;
+export const JWT_SECRET = process.env.JWT_SECRET || "dev";
 export const PORT = Number(process.env.PORT ?? '3000');
 export const isDevelopment = process.env.NODE_ENV === 'development';
 export const AUTH_SIGNUP_ENABLED = Boolean(process.env.AUTH_SIGNUP_ENABLED);
