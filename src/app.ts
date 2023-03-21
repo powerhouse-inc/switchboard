deeb4110b48aebb3d908ddaea5de7ecccd1de6c0
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-21 15:16:33 +0100

    unused deps

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import type { Express } from 'express';
-import cors from 'cors';
 import express from 'express';
 import expressPlayground from 'graphql-playground-middleware-express';
 import { getChildLogger } from './logger';
@@ -9,8 +8,6 @@ const logger = getChildLogger({ msgPrefix: 'APP' });
 export const createApp = (): Express => {
   logger.debug('Creating app');
   const app = express();
-
-  app.use(cors());
 
   app.get('/', expressPlayground({ endpoint: '/graphql' }));
 
