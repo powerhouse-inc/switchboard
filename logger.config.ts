df042dc56071664aa29cf027958251938866de8b
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-21 15:27:04 +0100

    debug command

--- logger.config.ts
+++ logger.config.ts
@@ -1,6 +1,6 @@
 import { LoggerConfig } from './src/types';
 
-export const loggerConfig: LoggerConfig = {
+export const defaultLoggerConfig: LoggerConfig = {
   // Filter by module name
   moduleFilter: [],
   // Filter by log prefix, e.g. adding
@@ -13,3 +13,22 @@ export const loggerConfig: LoggerConfig = {
   // Lowest printed log level of express logger
   httpLogLevel: 'info',
 };
+
+export const debugLogConfig: LoggerConfig = {
+  // Filter by module name
+  moduleFilter: [],
+  // Filter by log prefix, e.g. adding
+  // `pref` to filter is going to correspond to showing `[PERF]` logs
+  prefixFilter: [],
+  // Lowest printed log level of default logger
+  logLevel: 'debug',
+  // Lowest printed log level of prisma logger
+  dbLogLevel: ['query'],
+  // Lowest printed log level of express logger
+  httpLogLevel: 'debug',
+};
+
+const getLoggerConfig = (debug: boolean): LoggerConfig => {
+  return debug ? debugLogConfig : defaultLoggerConfig;
+}
+export const loggerConfig = getLoggerConfig(!!process.env.DEBUG);
