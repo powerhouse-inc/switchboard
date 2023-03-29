62eda6ff06ecb052a7702920cc29055b1531e872
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-29 18:15:37 +0200

    tables

--- api/tests/helpers/database.ts
+++ api/tests/helpers/database.ts
@@ -1,11 +1,26 @@
 import { beforeEach, afterEach } from 'vitest';
 import prisma from '../../src/database';
 
+let tableNameList: string[] | undefined;
+
+const formatTableNameToPrismaName = (tableName: string) => tableName.charAt(0).toLowerCase() + tableName.slice(1);
+
+const getTableNames = async () => {
+  if (typeof tableNameList !== 'undefined') {
+    return tableNameList;
+  }
+  const tableNames = await prisma.$queryRaw<{ name: string }[]>`SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';`;
+  tableNameList = tableNames.map((table) => formatTableNameToPrismaName(table.name));
+  return tableNameList;
+};
+
 export function cleanDatabase() {
   const clean = async () => {
-    await prisma.coreUnit.deleteMany();
-    await prisma.user.deleteMany();
-    await prisma.session.deleteMany();
+    const tablenames = await getTableNames();
+    for (const tableName of tablenames) {
+      const table: any = prisma[tableName as any];
+      await table.deleteMany();
+    }
   };
   beforeEach(async () => {
     await clean();
