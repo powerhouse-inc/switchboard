c84606b92cccf0d274b79ba8d32c8df3ae0d7b1e
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-29 11:26:54 +0200

    lint

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -1,21 +1,26 @@
-import ms from 'ms';
-import { sign, verify as jwtVerify } from 'jsonwebtoken';
-import { JWT_SECRET, JWT_EXPIRATION_PERIOD } from '../env';
-import { GraphQLError } from 'graphql';
+import ms from "ms";
+import { sign, verify as jwtVerify } from "jsonwebtoken";
+import { GraphQLError } from "graphql";
+import { JWT_SECRET, JWT_EXPIRATION_PERIOD } from "../env";
 
-export const format = (token: string) => `${token.slice(0, 3)}...${token.slice(-3)}`;
+export const format = (token: string) =>
+  `${token.slice(0, 3)}...${token.slice(-3)}`;
 
 /** Generate a JWT token
  * If expiryDurationSeconds is null, the token will never expire
  * If expiryDurationSeconds is undefined, the token will expire after the default expiry period
  */
-export const generate = (sessionId: string, expiryDurationSeconds?: number | null) => {
+export const generate = (
+  sessionId: string,
+  expiryDurationSeconds?: number | null
+) => {
   if (expiryDurationSeconds === null) {
     return sign({ sessionId }, JWT_SECRET);
   }
-  const expiresIn = typeof expiryDurationSeconds !== 'undefined'
-    ? ms(expiryDurationSeconds * 1000)
-    : JWT_EXPIRATION_PERIOD;
+  const expiresIn =
+    typeof expiryDurationSeconds !== "undefined"
+      ? ms(expiryDurationSeconds * 1000)
+      : JWT_EXPIRATION_PERIOD;
   return sign({ sessionId }, JWT_SECRET, { expiresIn });
 };
 
@@ -23,22 +28,22 @@ export const getExpiryDate = (expiryDurationSeconds?: number | null) => {
   if (expiryDurationSeconds === null) {
     return null;
   }
-  const expiresIn = typeof expiryDurationSeconds !== 'undefined'
-    ? expiryDurationSeconds * 1000
-    : ms(JWT_EXPIRATION_PERIOD);
+  const expiresIn =
+    typeof expiryDurationSeconds !== "undefined"
+      ? expiryDurationSeconds * 1000
+      : ms(JWT_EXPIRATION_PERIOD);
   return new Date(Date.now() + expiresIn);
 };
 
-export const verify = (token: string): { sessionId: string } => {
-  return jwtVerify(token, JWT_SECRET, (err, decoded) => {
+export const verify = (token: string): { sessionId: string } =>
+  jwtVerify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
       throw new GraphQLError(
-        err.name === 'TokenExpiredError'
-          ? 'Token expired'
-          : 'Invalid authentication token',
-        { extensions: { code: 'AUTHENTICATION_TOKEN_ERROR' } },
+        err.name === "TokenExpiredError"
+          ? "Token expired"
+          : "Invalid authentication token",
+        { extensions: { code: "AUTHENTICATION_TOKEN_ERROR" } }
       );
     }
     return decoded;
   }) as any;
-}
