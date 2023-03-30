a80d27677dd6912abb28d38b3c30373613c26f2f
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-30 15:37:46 +0200

    revert db cleaner

--- api/tests/helpers/database.ts
+++ api/tests/helpers/database.ts
@@ -1,31 +1,14 @@
 import { beforeEach, afterEach } from 'vitest';
 import prisma from '../../src/database';
 
-let tableNameList: string[] | undefined;
-
-function formatTableNameToPrismaName(tableName: string) {
-  return tableName.charAt(0).toLowerCase() + tableName.slice(1);
-}
-
-const getTableNames = async () => {
-  if (typeof tableNameList !== 'undefined') {
-    return tableNameList;
-  }
-  const tableNames = (
-    await prisma.$queryRaw<{ name: string }[]>`SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';`
-  );
-  tableNameList = tableNames.map((table) => formatTableNameToPrismaName(table.name));
-  return tableNameList;
-};
-
 export function cleanDatabase() {
   const clean = async () => {
-    const tablenames = await getTableNames();
-    await tablenames.slice(1).reduce(async (acc, tableName) => {
-      const table: any = prisma[tableName as any];
-      return acc.then(() => table.deleteMany());
-    }, (prisma[tablenames[0] as any] as any).deleteMany() as Promise<void>);
-    // can't type properly tablenames received via raw query.
+    // TODO: when migrate to postgres,
+    // replace below with
+    // https://www.prisma.io/docs/concepts/components/prisma-client/crud#deleting-all-data-with-raw-sql--truncate
+    await prisma.session.deleteMany();
+    await prisma.user.deleteMany();
+    await prisma.coreUnit.deleteMany();
   };
   beforeEach(async () => {
     await clean();
