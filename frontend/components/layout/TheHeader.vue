0e6f82e1fe5c8781178d3dd112a56f2cb8bc76f4
Valia Fetisov <contact@valiafetisov.com>
2023-03-30 12:20:07 +0200

    add github link

--- frontend/components/layout/TheHeader.vue
+++ frontend/components/layout/TheHeader.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="flex items-center justify-between bg-secondary px-1">
-    <div class="flex h-full flex-1">
+    <div class="flex-1 flex h-full">
       <nuxt-link to="/" class="flex h-full items-center gap-1">
         <img src="~/assets/logo.png" class="h-full py-1">
       </nuxt-link>
     </div>
     <LayoutTheMenu class="flex-shrink-0" />
-    <div class="flex-1" />
+    <div class="flex-1 flex h-full justify-end items-center pr-1">
+      <a :href="repository.url" target="_blank" class="hover:text-primary transition duration-300">
+        <NIcon size="30">
+          <LogoGithub />
+        </NIcon>
+      </a>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { LogoGithub } from '@vicons/ionicons5'
+import { repository } from '~/package.json'
+</script>
