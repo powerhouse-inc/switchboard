7d4f7dcd7afc8bea3cddc0bf8532ae3c4495e49f
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-03-30 17:07:06 +0200

    add health to api

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,19 +2,38 @@ import type { Express } from 'express';
 import express from 'express';
 import expressPlayground from 'graphql-playground-middleware-express';
 import { getChildLogger } from './logger';
+import prisma from './database';
 
 const logger = getChildLogger({ msgPrefix: 'APP' });
+const startupTime = new Date();
 
 export const createApp = (): Express => {
   logger.debug('Creating app');
   const app = express();
 
-  app.get('/', expressPlayground({
-    endpoint: '/api/graphql',
-    settings: {
-      'editor.theme': 'light',
-    },
-  }));
+  app.get('/healthz', async (_req, res) => {
+    try {
+      await prisma.$queryRaw`SELECT 1;`;
+    } catch (error) {
+      // TODO: test after migration to postgres, since sqlite will
+      // still return success if the file is missing
+      res.status(500).send('DB failed initialization check');
+    }
+    return res.json({
+      status: 'healthy',
+      time: new Date(),
+      startupTime,
+    });
+  });
+  app.get(
+    '/',
+    expressPlayground({
+      endpoint: '/api/graphql',
+      settings: {
+        'editor.theme': 'light',
+      },
+    }),
+  );
 
   return app;
 };
