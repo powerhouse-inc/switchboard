82ce7f5e1aa71fc0286ac8ec6bad9eddc40e6bbd
Valia Fetisov <contact@valiafetisov.com>
2023-03-30 13:20:08 +0200

    use light theme for graphql playground

--- api/src/app.ts
+++ api/src/app.ts
@@ -9,7 +9,12 @@ export const createApp = (): Express => {
   logger.debug('Creating app');
   const app = express();
 
-  app.get('/', expressPlayground({ endpoint: '/api/graphql' }));
+  app.get('/', expressPlayground({
+    endpoint: '/api/graphql',
+    settings: {
+      'editor.theme': 'light',
+    },
+  }));
 
   return app;
 };
