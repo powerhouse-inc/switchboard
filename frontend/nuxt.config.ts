346a62c5d3fcced9ad9def80879f7150916438d0
Valia Fetisov <contact@valiafetisov.com>
2023-04-03 13:41:00 +0200

    revert preflight.css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/preflight.css'],
   app: {
     head: {
       title: 'Switchboard API',
--- frontend/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-// disabling preflight to manually add preflight style
-// else gives weird style overwriting
-// to get more information check out https://github.com/tailwindlabs/tailwindcss/issues/6602
-module.exports = {
-  corePlugins: {
-    preflight: false
-  }
-}
