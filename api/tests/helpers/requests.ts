7c7fbaa7a44f62ea0a93ec6c77bc6fa1cdd5a196
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-04-04 15:13:45 +0200

    remove deps

--- api/tests/helpers/requests.ts
+++ api/tests/helpers/requests.ts
@@ -1,46 +1,7 @@
-import builder from 'gql-query-builder';
 import { Client } from '../../generated';
 
 export const USERNAME = 'usernameTest';
 export const PASSWORD = 'passwordTest';
-
-export const getSignUpMutation = (
-  username: string = USERNAME,
-  password: string = PASSWORD,
-) => builder.mutation({
-  operation: 'signUp',
-  variables: {
-    user: {
-      value: {
-        username,
-        password,
-      },
-      type: 'UserNamePassInput',
-      required: true,
-    },
-  },
-  fields: ['session{id, referenceExpiryDate, isUserCreated}', 'token'],
-});
-
-export const signInMutation = builder.mutation({
-  operation: 'signIn',
-  variables: {
-    user: {
-      value: {
-        username: USERNAME,
-        password: PASSWORD,
-      },
-      type: 'UserNamePassInput',
-      required: true,
-    },
-  },
-  fields: ['session{id, referenceExpiryDate, isUserCreated}', 'token'],
-});
-
-export const meQuery = builder.query({
-  operation: 'me',
-  fields: ['id', 'username'],
-});
 
 export const signUp = async (
   client: Client,
