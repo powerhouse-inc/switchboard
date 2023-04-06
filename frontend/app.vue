37e8e5f13897e9523aa9eb2b159689e0ea6633d7
Valia Fetisov <contact@valiafetisov.com>
2023-04-06 16:09:14 +0200

    fixed header

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,8 +1,8 @@
 <template>
   <NMessageProvider>
     <div class="h-screen flex flex-col">
-      <LayoutTheHeader :user="user" :is-authorized="isAuthorized" class="flex-shrink-0 h-14" />
-      <div class="flex-grow">
+      <LayoutTheHeader :user="user" :is-authorized="isAuthorized" class="flex-shrink-0 h-14 fixed top-0 z-10" />
+      <div class="flex-grow mt-14">
         <NuxtPage :user="user" />
       </div>
     </div>
--- frontend/components/layout/TheHeader.vue
+++ frontend/components/layout/TheHeader.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="flex items-center justify-between bg-secondary px-1">
+  <div class="w-full flex items-center justify-between bg-secondary px-1">
     <div class="flex-1 flex h-full">
       <nuxt-link to="/" class="LogoLink flex h-full items-center gap-2 py-1">
         <img src="~/assets/logo.png" class="h-full flex-shrink-0">
         <h2 class="text-primary opacity-0 transition duration-300 whitespace-nowrap hidden md:block">
-          Switchboard API
+          <n-button
+            secondary
+            round
+            type="primary"
+            class="gap-1 !pl-2 px-0 lg:!pl-5"
+            icon-placement="right"
+          >
+            Switchboard API
+          </n-button>
         </h2>
       </nuxt-link>
     </div>
     <LayoutTheMenu class="flex-shrink-0" />
     <div class="flex-1 flex h-full justify-end items-center pr-1 gap-1">
       <nuxt-link v-if="isAuthorized" to="/user">
-        <n-button secondary round type="primary" class="gap-1 !pl-2 px-0 lg:!pl-5" icon-placement="right">
-          <span class="hidden lg:block">Logged in as <strong>{{ user?.username }}</strong></span>
+        <n-button
+          secondary
+          round
+          type="primary"
+          class="gap-1 !pl-2 px-0 lg:!pl-5"
+          icon-placement="right"
+        >
+          <span class="hidden lg:block capitalize">{{ user?.username }}</span>
           <template #icon>
             <n-icon :component="PersonCircleOutline" />
           </template>
