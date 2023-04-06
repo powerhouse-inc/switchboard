1bce5947d6a95de2a5b490cc5017703ebd7bfcd4
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-04-06 10:27:37 +0200

    add generation script

---
+++ frontend/scripts/generateDocs.ts
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+import { concatMdSync } from 'concat-md'
+import {mkdirpSync} from 'mkdirp'
+
+function generateMdDocs (pathToDir: string, outputFilePath: string) {
+  const result = concatMdSync(pathToDir, {
+    fileNameAsTitle: true,
+    dirNameAsTitle: true
+  })
+  mkdirpSync(path.dirname(outputFilePath))
+  fs.writeFileSync(outputFilePath, result, {encoding: 'utf8'})
+}
+
+function main () {
+  const pathToDir = path.resolve(__dirname, '..', 'docs')
+  const outputFilePath = path.resolve(__dirname, '..', 'content', 'docs', 'index.md')
+  generateMdDocs(pathToDir, outputFilePath)
+}
+
+main()
