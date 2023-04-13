c1818fa8b13808ff18486584911f44ed7118ce65
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-04-13 17:50:01 +0200

    lint

--- api/src/helpers/origin.ts
+++ api/src/helpers/origin.ts
@@ -16,7 +16,10 @@ export function isOriginValid(originParam: string): boolean {
     return true;
   });
 }
-export function throwGQLErrorIfOriginDisallowed(originRestriction: string, originReceived?: string) {
+export function throwGQLErrorIfOriginDisallowed(
+  originRestriction: string,
+  originReceived?: string,
+) {
   if (originRestriction !== '*') {
     if (!originReceived) {
       throw new GraphQLError('Origin not provided', {
