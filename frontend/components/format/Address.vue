a42ec5e125c57eef512a680f77c1cd142dbe6195
Valia Fetisov <contact@valiafetisov.com>
2023-06-26 10:29:08 +0200

    frontend: add user address to the header

---
+++ frontend/components/format/Address.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>{{ shortenAddress }}</span>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  address: {
+    type: String,
+    default: ''
+  }
+})
+
+const TRIM_LENGTH = 4
+
+const shortenAddress = computed(() => {
+  if (!props.address) {
+    return ''
+  }
+  const start = props.address.substr(0, TRIM_LENGTH + 1)
+  const end = props.address.substr(-TRIM_LENGTH)
+  return `${start}...${end}`
+})
+</script>
