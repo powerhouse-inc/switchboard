b6bff038fbb66f35190a12861eb99ec1a32be79e
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-06-29 11:09:35 +0200

    genearate docs with version file

--- frontend/scripts/generateDocs.ts
+++ frontend/scripts/generateDocs.ts
@@ -1,8 +1,11 @@
 import fs from 'fs'
 import path from 'path'
 import { concatMdSync } from 'concat-md'
+import { execSync } from 'child_process'
 
 const DOCS_DIR = path.resolve(__dirname, '..', 'node_modules', '@acaldas', 'document-model-libs', 'dist', 'docs')
+const PACAKGE_NAME = '@acaldas/document-model-libs'
+const DOCS_VERSION = execSync(`npm view ${PACAKGE_NAME} version`).toString()
 
 function generateMdDocs (pathToDir: string, outputFilePath: string) {
   const content = concatMdSync(pathToDir, {
@@ -16,8 +19,14 @@ function generateMdDocs (pathToDir: string, outputFilePath: string) {
 }
 
 function main () {
-  const outputFilePath = path.resolve(__dirname, '..', 'content', 'documentation', 'index.md')
-  generateMdDocs(DOCS_DIR, outputFilePath)
+  const dirname = path.resolve(__dirname, '..', 'content', 'documentation')
+  const outputFilePath = path.resolve(dirname, 'index.md')
+  const outputVersionPath = path.resolve(dirname, '.version')
+  if (!fs.existsSync(outputVersionPath) || fs.readFileSync(outputVersionPath, { encoding: 'utf8' }).toString() !== DOCS_VERSION) {
+    console.info('Generating docs...')
+    generateMdDocs(DOCS_DIR, outputFilePath)
+  }
+  fs.writeFileSync(outputVersionPath, DOCS_VERSION, { encoding: 'utf8' })
 }
 
 main()
