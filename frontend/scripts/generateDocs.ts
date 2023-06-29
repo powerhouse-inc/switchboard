c85200f4897bfd60478ef26097acbcab95dab0a3
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-06-29 10:40:44 +0200

    update script to use the package docs

--- frontend/scripts/generateDocs.ts
+++ frontend/scripts/generateDocs.ts
@@ -2,7 +2,8 @@ import fs from 'fs'
 import path from 'path'
 import { concatMdSync } from 'concat-md'
 
-const DOCS_DIR = process.env.DOCS_DIR
+const DOCS_DIR = path.resolve(__dirname, '..', 'node_modules', '@acaldas', 'document-model-libs', 'dist', 'docs')
+console.log(DOCS_DIR);
 
 function generateMdDocs (pathToDir: string, outputFilePath: string) {
   const content = concatMdSync(pathToDir, {
@@ -13,12 +14,8 @@ function generateMdDocs (pathToDir: string, outputFilePath: string) {
 }
 
 function main () {
-  if (!DOCS_DIR) {
-    throw new Error('Please provide DOCS_DIR env variable which points to documentation directory')
-  }
-  const pathToDir = path.resolve(DOCS_DIR)
   const outputFilePath = path.resolve(__dirname, '..', 'content', 'documentation', 'index.md')
-  generateMdDocs(pathToDir, outputFilePath)
+  generateMdDocs(DOCS_DIR, outputFilePath)
 }
 
 main()
