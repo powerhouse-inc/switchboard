5d07ba08023a0b7cdabc6b8ac8cb70cba4027868
Valia Fetisov <contact@valiafetisov.com>
2023-06-29 09:55:11 +0200

    frontend: throw correct error message if no extension is present

--- frontend/composables/useWallet.ts
+++ frontend/composables/useWallet.ts
@@ -1,13 +1,24 @@
 import { providers, utils } from 'ethers'
 
+declare global {
+  interface Window {
+    ethereum?: providers.ExternalProvider;
+  }
+}
+
 const walletAddress = ref(undefined as string | undefined)
 const isConnected = computed(() => Boolean(walletAddress.value))
 
 const useWallet = function () {
-  const provider = new providers.Web3Provider((window as any).ethereum)
+  const getProvider = () => {
+    if (!window.ethereum) {
+      throw new Error('Please install web-based wallet such as Metamask')
+    }
+    return new providers.Web3Provider(window.ethereum)
+  }
 
   const connectWallet = async function () {
-    const addresses = await provider.send('eth_requestAccounts', [])
+    const addresses = await getProvider().send('eth_requestAccounts', [])
     walletAddress.value = utils.getAddress(addresses[0])
     return walletAddress.value
   }
@@ -16,7 +27,7 @@ const useWallet = function () {
     if (!walletAddress.value) {
       throw new Error('Wallet is not connected')
     }
-    const signature = await provider.send('personal_sign', [
+    const signature = await getProvider().send('personal_sign', [
       message, walletAddress.value.toLowerCase()
     ])
     return signature
