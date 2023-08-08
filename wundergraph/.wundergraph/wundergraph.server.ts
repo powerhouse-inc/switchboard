5a98d7ae74dfddd197e42b823d8ce7d5222d2559
KirillDogadin-std <kirillsc2@gmail.com>
2023-08-08 11:45:51 +0200

    wip trying to make docker setup work

--- wundergraph/.wundergraph/wundergraph.server.ts
+++ wundergraph/.wundergraph/wundergraph.server.ts
@@ -2,11 +2,7 @@ import { configureWunderGraphServer } from '@wundergraph/sdk/server';
 
 export default configureWunderGraphServer(() => ({
 	hooks: {
-		queries: {
-			Countries: {
-				preResolve: async ({ operations }) => {},
-			},
-		},
+		queries: {},
 		mutations: {},
 	},
 }));
