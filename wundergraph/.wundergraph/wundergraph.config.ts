efb5ca5128cc0bd900b27415bc2774ded21a3c39
KirillDogadin-std <kirillsc2@gmail.com>
2023-08-09 09:38:36 +0200

    add dotenv package

--- wundergraph/.wundergraph/wundergraph.config.ts
+++ wundergraph/.wundergraph/wundergraph.config.ts
@@ -1,15 +1,24 @@
 import { configureWunderGraphApplication, cors, EnvironmentVariable, introspect, templates } from '@wundergraph/sdk';
 import server from './wundergraph.server';
 import operations from './wundergraph.operations';
+import dotenv from 'dotenv';
+
+dotenv.config();
+if (!process.env.ECOSYSTEM_URL) {
+  throw new Error('ECOSYSTEM_URL environment variable is not set');
+}
+if (!process.env.SWITCHBOARD_URL) {
+  throw new Error('SWITCHBOARD_URL environment variable is not set');
+}
 
 const ecosystem = introspect.graphql({
 	apiNamespace: 'ecosystem',
-	url: process.env.ECOSYSTEM_URL || 'https://countries.trevorblades.com/graphql',
+	url: process.env.ECOSYSTEM_URL,
 });
 
 const switchboard = introspect.graphql({
 	apiNamespace: '',
-	url: process.env.SWITCHBOARD_URL || 'http://localhost:3001/graphql',
+	url: process.env.SWITCHBOARD_URL,
   headers: (builder) => builder.addClientRequestHeader('Authorization', 'Authorization')
 });
 
