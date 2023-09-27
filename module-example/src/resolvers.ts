6272f83c82d7f7ff19375f96a9a144680ef2f638
KirillDogadin-std <kirillsc2@gmail.com>
2023-09-27 14:16:27 +0200

    typecheck

--- module-example/src/resolvers.ts
+++ module-example/src/resolvers.ts
@@ -1,11 +1,16 @@
+import { PrismaClient } from '@prisma/client';
 import { queryField, nonNull } from 'nexus/dist';
+
+interface Context {
+  prisma: PrismaClient;
+}
 
 export const countUsers = queryField('countUsers', {
   type: 'Counter',
   args: {
     message: nonNull('String'),
   },
-  resolve: async (_root, args, ctx) => {
+  resolve: async (_root, args, ctx: Context) => {
     const aggr = await ctx.prisma.user.aggregate({ _count: { address: true } });
     const { message } = args;
     return {
@@ -17,5 +22,5 @@ export const countUsers = queryField('countUsers', {
 
 export const countUsersPrisma = queryField('countUsersPrisma', {
   type: 'Counter',
-  resolve: async (_root, _args, ctx) => ctx.prisma.user.doCount(),
+  resolve: async (_root, _args, ctx: Context) => ctx.prisma.user.doCount(),
 });
