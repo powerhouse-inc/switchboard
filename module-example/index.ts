bd2922d86ec04be6bb6adfdaf87719a354694219
KirillDogadin-std <kirillsc2@gmail.com>
2023-09-29 16:17:34 +0200

    use prisma extension

--- module-example/index.ts
+++ module-example/index.ts
@@ -1,22 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import { prismaExtension } from './prisma/extesion';
 import * as types from './src/index';
 
-const getUserCrud = (prisma: PrismaClient) => ({
-  doCount: async () => {
-    const aggr = await prisma.user.aggregate({ _count: { address: true } });
-    const message = 'Hello from Prisma';
-    return {
-      count: aggr._count.address,
-      message,
-    };
-  },
-});
-
 export const setup = (prisma: PrismaClient) => {
-  const extended = prisma.$extends({
-    model: {
-      user: getUserCrud(prisma),
-    },
-  });
+  const extended = prisma.$extends(prismaExtension);
   return { types, prisma: extended };
 };
