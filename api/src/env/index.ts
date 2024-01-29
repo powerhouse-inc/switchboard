d6fdd6e1ace50012c8819faec2e4bc48c1e3f3a9
Frank <frank.pfeift@protonmail.com>
2024-01-29 15:44:02 +0100

    feat: improve heroku setup

--- api/src/env/index.ts
+++ api/src/env/index.ts
@@ -1,12 +1,13 @@
-import dotenv from 'dotenv';
-import { getJwtSecret, getJwtExpirationPeriod } from './getters';
+import dotenv from "dotenv";
+import { getJwtSecret, getJwtExpirationPeriod } from "./getters";
 
 dotenv.config();
 
 export const JWT_SECRET = getJwtSecret();
-export const PORT = Number(process.env.PORT ?? '3001');
-export const isDevelopment = process.env.NODE_ENV === 'development';
+export const PORT = 3000;
+export const isDevelopment = process.env.NODE_ENV === "development";
 export const AUTH_SIGNUP_ENABLED = Boolean(process.env.AUTH_SIGNUP_ENABLED);
 export const JWT_EXPIRATION_PERIOD: string = getJwtExpirationPeriod();
 export const API_ORIGIN = process.env.API_ORIGIN || `http://0.0.0.0:${PORT}`;
-export const API_GQL_ENDPOINT = process.env.API_GQL_ENDPOINT || `${API_ORIGIN}/graphql`;
+export const API_GQL_ENDPOINT =
+  process.env.API_GQL_ENDPOINT || `${API_ORIGIN}/graphql`;
