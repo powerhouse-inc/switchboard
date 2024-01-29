d45d4529c076214f3bb3fcdbd45f7a56b3d9265e
Frank <frank.pfeift@protonmail.com>
2024-01-29 16:24:01 +0100

    fix: set drive id from context

--- api/src/modules/DocumentDrive/queries/drive.ts
+++ api/src/modules/DocumentDrive/queries/drive.ts
@@ -1,14 +1,11 @@
-import { nonNull, queryField } from 'nexus';
-import { DocumentDriveState } from '../definitions';
+import { nonNull, queryField } from "nexus";
+import { DocumentDriveState } from "../definitions";
 
-export const getDrive = queryField('drive', {
+export const getDrive = queryField("drive", {
   type: DocumentDriveState,
-  args: {
-    id: nonNull('String'),
-  },
-  resolve: async (_parent, { id }, ctx) => {
+  resolve: async (_parent, args, ctx) => {
     try {
-      const drive = await ctx.prisma.document.getDrive(id);
+      const drive = await ctx.prisma.document.getDrive(ctx.driveId ?? "1");
       return drive.global;
     } catch (e) {
       return null;
