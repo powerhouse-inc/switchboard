5adebdbe269145bc44f0d4c52c3b3bc44a3a41a8
Frank <frank.pfeift@protonmail.com>
2024-01-30 13:17:05 +0100

    fix: set proper port on wundergraph

--- wundergraph/.wundergraph/wundergraph.server.ts
+++ wundergraph/.wundergraph/wundergraph.server.ts
@@ -4,7 +4,7 @@ export default configureWunderGraphServer(() => ({
   options: {
     listen: {
       host: new EnvironmentVariable('SERVER_HOST', '0.0.0.0'),
-      port: new EnvironmentVariable('SERVER_PORT', '3003'),
+      port: new EnvironmentVariable('SERVER_PORT', process.env.PORT ?? '3003'),
     },
     logger: {
       level: new EnvironmentVariable<LoggerLevel>('SERVER_LOG_LEVEL', 'debug'),
