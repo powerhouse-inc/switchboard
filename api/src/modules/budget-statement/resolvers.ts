4e6695fa57fae12b97234411ffbde8c6bc6f5fda
Frank <frank.pfeift@protonmail.com>
2024-02-09 17:42:03 +0100

    feat: added budget statement document

--- api/src/modules/budget-statement/resolvers.ts
+++ api/src/modules/budget-statement/resolvers.ts
@@ -1,229 +1,141 @@
-import { enumType, objectType, queryField, unionType } from 'nexus';
+import { enumType, inputObjectType, objectType, queryField, unionType } from 'nexus';
 import { documentModelInterface } from '../document';
-import { GQLDateBase } from '../../graphql/server/drive/dateSchema';
-
-export const rwaDocument = objectType({
-  name: 'RwaDocument',
-  definition(t) {
-    t.implements(documentModelInterface);
-    t.nonNull.string('field1');
-  },
-});
-
+import { GQLAttachmentBase, GQLDateBase } from '../system';
 export const Account = objectType({
-  name: 'Account',
+  name: "BudgetStatementAccount",
   definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.string('reference');
-    t.string('label');
-  },
+    t.nonNull.string("address")
+    t.nonNull.string("name")
+    t.nonNull.list.nonNull.field("lineItems", { type: LineItem })
+  }
+})
+export const AuditReport = objectType({
+  name: "AuditReport",
+  definition(t) {
+    t.nonNull.field("timestamp", { type: GQLDateBase })
+    t.nonNull.field("report", { type: GQLAttachmentBase })
+    t.nonNull.field("status", { type: AuditReportStatus })
+  }
+})
+
+export const BudgetStatementState = objectType({
+  name: "BudgetStatementState",
+  definition(t) {
+    t.field("owner", { type: Owner })
+    t.string("month")
+    t.string("quoteCurrency")
+    t.nonNull.list.nonNull.field("accounts", { type: Account })
+    t.nonNull.list.nonNull.field("vesting", { type: Vesting })
+    t.field("ftes", { type: Ftes })
+    t.nonNull.list.nonNull.field("auditReports", { type: AuditReport })
+    t.nonNull.list.nonNull.field("comments", { type: Comment })
+  }
+})
+export const Comment = objectType({
+  name: "Comment",
+  definition(t) {
+    t.nonNull.string("key")
+    t.nonNull.field("author", { type: CommentAuthor })
+    t.nonNull.string("comment")
+    t.nonNull.field("timestamp", { type: GQLDateBase })
+    t.nonNull.field("status", { type: BudgetStatus })
+  }
+})
+export const CommentAuthor = objectType({
+  name: "CommentAuthor",
+  definition(t) {
+    t.string("ref")
+    t.string("id")
+    t.string("username")
+    t.string("roleLabel")
+  }
+})
+export const Ftes = objectType({
+  name: "Ftes",
+  definition(t) {
+    t.nonNull.float("value")
+    t.nonNull.list.nonNull.field("forecast", { type: FtesForecast })
+  }
+})
+export const FtesForecast = objectType({
+  name: "FtesForecast",
+  definition(t) {
+    t.nonNull.string("month")
+    t.nonNull.float("value")
+  }
+})
+export const LineItem = objectType({
+  name: "LineItem",
+  definition(t) {
+    t.field("group", { type: LineItemGroup })
+    t.nonNull.boolean("headcountExpense")
+    t.field("category", { type: LineItemCategory })
+    t.float("budgetCap")
+    t.float("actual")
+    t.float("payment")
+    t.nonNull.list.nonNull.field("forecast", { type: LineItemForecast })
+    t.string("comment")
+  }
+})
+export const LineItemCategory = objectType({
+  name: "LineItemCategory",
+  definition(t) {
+    t.nonNull.string("ref")
+    t.nonNull.string("id")
+    t.nonNull.string("title")
+  }
+})
+export const LineItemForecast = objectType({
+  name: "LineItemForecast",
+  definition(t) {
+    t.nonNull.string("month")
+    t.nonNull.float("value")
+    t.nonNull.float("budgetCap")
+  }
+})
+export const LineItemGroup = objectType({
+  name: "LineItemGroup",
+  definition(t) {
+    t.nonNull.string("ref")
+    t.nonNull.string("id")
+    t.nonNull.string("title")
+    t.nonNull.string("color")
+  }
+})
+export const Owner = objectType({
+  name: "Owner",
+  definition(t) {
+    t.string("ref")
+    t.string("id")
+    t.string("title")
+  }
+})
+export const Vesting = objectType({
+  name: "Vesting",
+  definition(t) {
+    t.nonNull.string("key")
+    t.nonNull.string("date")
+    t.nonNull.string("amount")
+    t.nonNull.string("amountOld")
+    t.nonNull.string("comment")
+    t.nonNull.string("currency")
+    t.nonNull.boolean("vested")
+  }
+})
+
+export const AuditReportStatus = enumType({
+  name: "AuditReportStatus",
+  members: ['Approved', 'ApprovedWithComments', 'NeedsAction', 'Escalated'],
+});
+export const BudgetStatus = enumType({
+  name: "BudgetStatus",
+  members: ['Draft', 'Review', 'Final', 'Escalated'],
 });
 
-// export const CashGroupTransactionType = enumType({
-//   name: 'CashGroupTransactionType',
-//   members: ['PrincipalDraw', 'PrincipalReturn'],
-// });
-
-// export const FixedIncomeGroupTransactionType = enumType({
-//   name: 'FixedIncomeGroupTransactionType',
-//   members: ['AssetPurchase', 'AssetSale', 'InterestDraw', 'InterestReturn', 'FeesPayment'],
-// });
-
-// export const GroupTransactionType = unionType({
-//   name: 'GroupTransactionType',
-//   definition(t) {
-//     t.members(
-//       CashGroupTransactionType,
-//       FixedIncomeGroupTransactionType,
-//     );
-//   },
-//   resolveType: () => true,
-// });
-
-export const GroupTransactionType = objectType({
-  name: 'GroupTransactionType',
-  definition(t) {
-    t.string('name');
-  },
-});
-
-export const BaseTransaction = objectType({
-  name: 'BaseTransaction',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.id('assetId');
-    t.nonNull.float('amount');
-    t.nonNull.field('entryTime', { type: GQLDateBase });
-    t.field('tradeTime', { type: GQLDateBase });
-    t.field('settlementTime', { type: GQLDateBase });
-    t.string('txRef');
-    t.id('accountId');
-    t.id('counterPartyAccountId');
-  },
-});
-
-export const AssetPurchaseGroupTransaction = objectType({
-  name: 'AssetPurchaseGroupTransaction',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.field('type', { type: GroupTransactionType });
-    t.field('cashTransaction', { type: BaseTransaction });
-    t.field('fixedIncomeTransaction', { type: BaseTransaction });
-    t.list.field('feeTransactions', { type: BaseTransaction });
-  },
-});
-export const AssetSaleGroupTransaction = objectType({
-  name: 'AssetSaleGroupTransaction',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.field('type', { type: GroupTransactionType });
-    t.field('cashTransaction', { type: BaseTransaction });
-    t.field('fixedIncomeTransaction', { type: BaseTransaction });
-    t.list.field('feeTransactions', { type: BaseTransaction });
-  },
-});
-
-export const Cash = objectType({
-  name: 'Cash',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.id('spvId');
-    t.nonNull.string('currency');
-  },
-});
-export const FeesPaymentGroupTransaction = objectType({
-  name: 'FeesPaymentGroupTransaction',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.field('type', { type: GroupTransactionType });
-    t.list.field('feeTransactions', { type: BaseTransaction });
-  },
-});
-export const FixedIncome = objectType({
-  name: 'FixedIncome',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.id('fixedIncomeTypeId');
-    t.nonNull.string('name');
-    t.nonNull.id('spvId');
-    t.nonNull.field('maturity', { type: GQLDateBase });
-    t.nonNull.field('purchaseDate', { type: GQLDateBase });
-    t.nonNull.float('notional');
-    t.nonNull.float('purchasePrice');
-    t.nonNull.float('purchaseProceeds');
-    t.nonNull.float('totalDiscount');
-    t.nonNull.float('marketValue');
-    t.nonNull.float('annualizedYield');
-    t.nonNull.float('realizedSurplus');
-    t.nonNull.float('totalSurplus');
-    t.string('ISIN');
-    t.string('CUSIP');
-    t.float('coupon');
-    t.float('currentValue');
-  },
-});
-export const FixedIncomeType = objectType({
-  name: 'FixedIncomeType',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.string('name');
-  },
-});
-export const InterestDrawGroupTransaction = objectType({
-  name: 'InterestDrawGroupTransaction',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.field('type', { type: GroupTransactionType });
-    t.field('interestTransaction', { type: BaseTransaction });
-  },
-});
-export const InterestReturnGroupTransaction = objectType({
-  name: 'InterestReturnGroupTransaction',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.field('type', { type: GroupTransactionType });
-    t.field('interestTransaction', { type: BaseTransaction });
-  },
-});
-export const PrincipalDrawGroupTransaction = objectType({
-  name: 'PrincipalDrawGroupTransaction',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.field('type', { type: GroupTransactionType });
-    t.field('cashTransaction', { type: BaseTransaction });
-    t.list.field('feeTransactions', { type: BaseTransaction });
-  },
-});
-export const PrincipalReturnGroupTransaction = objectType({
-  name: 'PrincipalReturnGroupTransaction',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.field('type', { type: GroupTransactionType });
-    t.field('cashTransaction', { type: BaseTransaction });
-    t.list.field('feeTransactions', { type: BaseTransaction });
-  },
-});
-
-export const Spv = objectType({
-  name: 'Spv',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.string('name');
-  },
-});
-
-export const ServiceProvider = objectType({
-  name: 'ServiceProvider',
-  definition(t) {
-    t.nonNull.id('id');
-    t.nonNull.string('name');
-    t.nonNull.string('feeType');
-    t.nonNull.id('accountId');
-  },
-});
-
-export const Asset = unionType({
-  name: 'Asset',
-  definition(t) {
-    t.members(FixedIncome, Cash);
-  },
-  resolveType: () => true,
-});
-
-export const GroupTransaction = unionType({
-  name: 'GroupTransaction',
-  definition(t) {
-    t.members(
-      PrincipalDrawGroupTransaction,
-      PrincipalReturnGroupTransaction,
-      AssetPurchaseGroupTransaction,
-      AssetSaleGroupTransaction,
-      InterestDrawGroupTransaction,
-      InterestReturnGroupTransaction,
-      FeesPaymentGroupTransaction,
-    );
-  },
-  resolveType: () => true,
-});
-
-export const RealWorldAssetsState = objectType({
-  name: 'RealWorldAssetDocument',
+export const BudgetStatementDocument = objectType({
+  name: 'BudgetStatement',
   definition(t) {
     t.implements(documentModelInterface);
-    t.nonNull.list.nonNull.field('accounts', { type: Account });
-    t.nonNull.id('principalLenderAccountId');
-    t.nonNull.list.nonNull.field('spvs', { type: Spv });
-    t.nonNull.list.nonNull.field('feeTypes', { type: ServiceProvider });
-    t.nonNull.list.nonNull.field('fixedIncomeTypes', { type: FixedIncomeType });
-    t.nonNull.list.nonNull.field('portfolio', { type: Asset });
-    t.nonNull.list.nonNull.field('transactions', { type: GroupTransaction });
-  },
-});
-
-export const TransactionFee = objectType({
-  name: 'TransactionFee',
-  definition(t) {
-    t.nonNull.id('serviceProviderId');
-    t.nonNull.float('amount');
+    t.nonNull.field('state', { type: BudgetStatementState });
   },
 });
