0f458f95043eda0554fe04e74c006481d33903f6
Frank <frank.pfeift@protonmail.com>
2024-02-16 12:15:37 +0100

    feat: replace landing page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="flex flex-col h-full px-5 md:px-10">
-    <div class="flex-shrink-0 text-center py-10 md:py-16">
-      <h1 class="text-3xl md:text-6xl font-bold">
-        Switchboard API
-      </h1>
-      <div class="text-sm md:text-base">
-        Open-source API over MakerDAO models
-      </div>
-    </div>
-    <div class="flex-grow flex items-center justify-center">
-      <div class="w-full h-full bg-white max-w-screen-2xl" />
-    </div>
+  <div class="h-full">
+    <iframe :src="'https://www.powerhouse.inc/switchboard'" class="w-full h-full" />
   </div>
 </template>
