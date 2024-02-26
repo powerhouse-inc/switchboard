489113993d419e7552923ce8760346b06d797d2c
Frank <frank.pfeift@protonmail.com>
2024-02-26 14:27:46 +0100

    feat: added staging

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_SWITCHBOARD_GRAPHQL_HOST: process.env.NEXT_PUBLIC_SWITCHBOARD_GRAPHQL_HOST || "https://ph-switchboard-nginx-prod-c84ebf8c6e3b.herokuapp.com",
-    },
+  env: {
+    NEXT_PUBLIC_SWITCHBOARD_GRAPHQL_HOST:
+      process.env.NEXT_PUBLIC_SWITCHBOARD_GRAPHQL_HOST ||
+      "https://ph-switchboard-nginx-prod-c84ebf8c6e3b.herokuapp.com",
+  },
 };
 
 export default nextConfig;
