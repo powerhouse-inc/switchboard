df00f05c873120ba67d5323bd06591e18a8d6455
Frank <frank.pfeift@protonmail.com>
2024-03-14 11:24:11 +0100

    feat: updated read model listener for rwa

--- api/src/modules/real-world-assets/model.ts
+++ api/src/modules/real-world-assets/model.ts
@@ -2,52 +2,65 @@ import type { Prisma } from '@prisma/client';
 import { transformPortfolioToState } from './utils';
 
 export function getRWACRUD(prisma: Prisma.TransactionClient) {
-  return {
-    findRWAPortfolios: async (where?: Prisma.RWAPortfolioWhereInput) => {
-      const newWhere: Prisma.RWAPortfolioWhereInput = {
-        ...where
-      }
-
-      try {
-        const portfolios = await prisma.rWAPortfolio.findMany({
-          include: {
-            accounts: {
-              include: {
-                account: true
-              }
-            },
-            feeTypes: {
-              include: {
-                spv: true
-              }
-            },
-            fixedIncomeTypes: {
-              include: {
-                fixedIncome: true
-              }
-            },
-            portfolio: {
-              include: {
-                fixedIncomeType: {
-                  select: {
-                    id: true,
-                    name: true
-                  }
-                }
-              }
-            },
-            spvs: {
-              include: {
-                spv: true
-              }
+    return {
+        findRWAPortfolios: async (where?: Prisma.RWAPortfolioWhereInput) => {
+            const newWhere: Prisma.RWAPortfolioWhereInput = {
+                ...where
             }
-          }
-        })
-        return transformPortfolioToState(portfolios)
-      } catch (e) {
-        console.error(e)
-        return [];
-      }
-    },
-  };
+
+            try {
+                const portfolios = await prisma.rWAPortfolio.findMany({
+                    include: {
+                        accounts: {
+                            include: {
+                                account: true
+                            }
+                        },
+                        feeTypes: {
+                            include: {
+                                spv: true
+                            }
+                        },
+                        fixedIncomeTypes: {
+                            include: {
+                                fixedIncome: true
+                            }
+                        },
+                        portfolio: {
+                            include: {
+                                fixedIncomeType: {
+                                    select: {
+                                        id: true,
+                                        name: true
+                                    }
+                                }
+                            }
+                        },
+                        spvs: {
+                            include: {
+                                spv: true
+                            }
+                        },
+                        RWAGroupTransaction: {
+                            include: {
+                                cashTransaction: true,
+                                feeTransactions: {
+                                    include: {
+                                        baseTransaction: true
+                                    }
+                                },
+                                fixedIncomeTransaction: true,
+                                interestTransaction: true,
+                                fees: true,
+                            }
+                        }
+                    }
+                })
+                return transformPortfolioToState(portfolios)
+            } catch (e) {
+                console.error(e)
+                return [];
+            }
+        },
+    };
 }
--- api/src/modules/real-world-assets/utils.ts
+++ api/src/modules/real-world-assets/utils.ts
@@ -1,67 +1,91 @@
 import { Prisma, RWAPortfolio } from "@prisma/client";
 
 export function transformPortfolioToState(portfolios: Prisma.RWAPortfolioGetPayload<{
-    include: {
-        accounts: {
-            include: {
-                account: true
-            }
-        },
-        feeTypes: {
-            include: {
-                spv: true
-            }
-        },
-        fixedIncomeTypes: {
-            include: {
-                fixedIncome: true
-            }
-        },
-        portfolio: {
-            include: {
-                fixedIncomeType: {
-                    select: {
-                        id: true,
-                        name: true
-                    }
-                }
-            }
-        },
-        spvs: {
-            include: {
-                spv: true
-            }
+  include: {
+    accounts: {
+      include: {
+        account: true
+      }
+    },
+    feeTypes: {
+      include: {
+        spv: true
+      }
+    },
+    fixedIncomeTypes: {
+      include: {
+        fixedIncome: true
+      }
+    },
+    portfolio: {
+      include: {
+        fixedIncomeType: {
+          select: {
+            id: true,
+            name: true
+          }
         }
+      }
+    },
+    spvs: {
+      include: {
+        spv: true
+      }
+    },
+    RWAGroupTransaction: {
+      include: {
+        cashTransaction: true,
+        feeTransactions: {
+          include: {
+            baseTransaction: true
+          }
+        },
+        fixedIncomeTransaction: true,
+        interestTransaction: true,
+        fees: true,
+      }
     }
+  }
 }>[]) {
-    return portfolios.map(portfolio => ({
-        id: portfolio.id,
-        // spvs: [],
-        spvs: portfolio.spvs.map(spv => ({
-            id: spv.spv.id,
-            name: spv.spv.name
-        })),
-        feeTypes: portfolio.feeTypes.map(feeType => ({
-            id: feeType.spv.id,
-            name: feeType.spv.name
-        })),
-        portfolio: portfolio.portfolio.map(asset => ({
-            id: asset.id,
-            purchasePrice: asset.purchasePrice,
-            purchaseDate: asset.purchaseDate,
-            name: asset.name,
-            fixedIncomeType: {
-                id: asset.fixedIncomeType?.id,
-                name: asset.fixedIncomeType?.name
-            }
-        })),
-        fixedIncomeTypes: portfolio.fixedIncomeTypes.map(fixedIncomeType => ({
-            id: fixedIncomeType.fixedIncome.id,
-            name: fixedIncomeType.fixedIncome.name
-        })),
-        accounts: portfolio.accounts.map(account => ({
-            id: account.account.id,
-            name: account.account.label
-        }))
-    }));
+  return portfolios.map(portfolio => ({
+    id: portfolio.id,
+    // spvs: [],
+    spvs: portfolio.spvs.map(spv => ({
+      id: spv.spv.id,
+      name: spv.spv.name
+    })),
+    feeTypes: portfolio.feeTypes.map(feeType => ({
+      id: feeType.spv.id,
+      name: feeType.spv.name
+    })),
+    portfolio: portfolio.portfolio.map(asset => ({
+      id: asset.id,
+      purchasePrice: asset.purchasePrice,
+      purchaseDate: asset.purchaseDate,
+      name: asset.name,
+      fixedIncomeType: {
+        id: asset.fixedIncomeType?.id,
+        name: asset.fixedIncomeType?.name
+      }
+    })),
+    fixedIncomeTypes: portfolio.fixedIncomeTypes.map(fixedIncomeType => ({
+      id: fixedIncomeType.fixedIncome.id,
+      name: fixedIncomeType.fixedIncome.name
+    })),
+    accounts: portfolio.accounts.map(account => ({
+      id: account.account.id,
+      name: account.account.label
+    })),
+    transactions: portfolio.RWAGroupTransaction.map(transaction => ({
+      id: transaction.id,
+      type: transaction.type,
+      cashTransaction: transaction.cashTransaction,
+      feeTransactions: transaction.feeTransactions.map(feeTransaction => ({
+        id: feeTransaction.baseTransaction.id,
+        amount: feeTransaction.baseTransaction.amount,
+      })),
+      fixedIncomeTransaction: transaction.fixedIncomeTransaction,
+      interestTransaction: transaction.interestTransaction
+    }))
+  }));
 }
