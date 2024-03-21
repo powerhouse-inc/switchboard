de287175add27ea64ba2cb14b5e6c9dbca0c3e23
Frank <frank.pfeift@protonmail.com>
2024-03-21 10:39:04 +0100

    feat: added base path to next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
       process.env.NEXT_PUBLIC_SWITCHBOARD_GRAPHQL_HOST ||
       "https://ph-switchboard-nginx-prod-c84ebf8c6e3b.herokuapp.com",
   },
+  basePath: process.env.BASE_PATH ?? undefined,
 };
 
 export default nextConfig;
