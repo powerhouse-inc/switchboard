3a731ace09116084e501192419affd1b43673547
Frank <frank.pfeift@protonmail.com>
2024-03-22 11:25:30 +0100

    fix: endpoint base path

--- api/src/app.ts
+++ api/src/app.ts
@@ -36,7 +36,7 @@ export const createApp = (): { app: Express, router: express.Router } => {
     (req, res) => {
       res.setHeader('Content-Type', 'text/html')
       const basePath = process.env.BASE_PATH === "/" ? "" : process.env.BASE_PATH || '';
-      const endpoint = basePath + req.params.driveId !== undefined ? `/d/${req.params.driveId}` : '/drives'
+      const endpoint = `${basePath}${req.params.driveId !== undefined ? `/d/${req.params.driveId}` : '/drives'}`
       res.send(renderPlaygroundPage({
         endpoint: endpoint,
         settings: {
