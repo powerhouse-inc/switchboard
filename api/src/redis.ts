66797009c31b93f8da891f854eb6cc68215131d4
Frank <frank.pfeift@protonmail.com>
2024-04-19 13:20:11 +0200

    chore: set proper redis endpoint

--- api/src/redis.ts
+++ api/src/redis.ts
@@ -6,14 +6,13 @@ export let redisClient: RedisClientType | null = null;
 export const getRedisClient = async () => {
   if (!redisClient) {
     redisClient = await createClient({
-      url: process.env.REDIS_URL,
+      url: process.env.REDIS_TLS_URL,
       socket: {
         tls: true,
         rejectUnauthorized: false,
       }
     }).on('error', (err: string) => console.log('Redis Client Error', err))
       .connect() as RedisClientType;
-
   }
 
   return redisClient;
