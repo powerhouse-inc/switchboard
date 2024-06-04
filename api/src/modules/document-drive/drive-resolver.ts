b3341966c8ce97fe014db2b16420d5f543cbdc94
acaldas <acaldas.dev@gmail.com>
2024-06-04 18:27:12 +0100

    feat: add pushUpdates strands to queue at the same time

--- api/src/modules/document-drive/drive-resolver.ts
+++ api/src/modules/document-drive/drive-resolver.ts
@@ -370,42 +370,32 @@ export const pushUpdates = mutationField('pushUpdates', {
     strands: list(nonNull(InputStrandUpdate)),
   },
   resolve: async (_parent, { strands }, ctx: Context) => {
-    logger.info('pushUpdates')
+    logger.info('pushUpdates');
     if (!strands || strands?.length === 0) return [];
 
     try {
-      const listenerRevisions: IListenerRevision[] = [];
-
-      const sortedStrands = strands.reduce(
-        (acc, curr) =>
-          curr.documentId ? [...acc, curr] : [curr, ...acc],
-        [] as typeof strands
-      );
-
-      for (const s of sortedStrands) {
-        const operations =
-          s.operations?.map((o) => ({
-            ...o,
-            input: JSON.parse(o.input),
-            skip: o.skip ?? 0,
-            scope: s.scope as OperationScope,
-            branch: "main",
-          })) ?? [];
+      const listenerRevisions: IListenerRevision[] = await Promise.all(strands.map(async (s) => {
+        const operations = s.operations?.map((o) => ({
+          ...o,
+          input: JSON.parse(o.input),
+          skip: o.skip ?? 0,
+          scope: s.scope as OperationScope,
+          branch: "main",
+        })) ?? [];
 
         const result = await ctx.prisma.document.pushUpdates(
           s.driveId,
           operations as Operation<DocumentDriveAction>[],
-          s.documentId ?? undefined
+          s.documentId ?? undefined,
         );
 
         if (result.status !== "SUCCESS") logger.error(result.error);
 
-        const revision =
-          result.document?.operations[s.scope as OperationScope]
-            .slice()
-            .pop()?.index ?? -1;
+        const revision = result.document?.operations[s.scope as OperationScope]
+          .slice()
+          .pop()?.index ?? -1;
 
-        listenerRevisions.push({
+        return {
           revision,
           branch: s.branch,
           documentId: s.documentId ?? "",
@@ -413,12 +403,17 @@ export const pushUpdates = mutationField('pushUpdates', {
           scope: s.scope as OperationScope,
           status: result.status,
           error: result.error?.message,
-        });
-      }
+        };
+      }));
 
       return listenerRevisions;
     } catch (e: any) {
-      throw new DocumentDriveError({ code: 500, message: e.message ?? "Failed to push updates", logging: true, context: e })
+      throw new DocumentDriveError({
+        code: 500,
+        message: e.message ?? "Failed to push updates",
+        logging: true,
+        context: e
+      });
     }
   },
 });
